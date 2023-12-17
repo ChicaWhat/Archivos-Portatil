926e6ce60ff159814ccae23cadb7121b090e109b
Carmen González <carmengh106@gmail.com>
2023-12-17 19:15:21 +0100

    Haciendo ejercicio 04 de la semana 4

--- Ejer04JS/script.js
+++ Ejer04JS/script.js
@@ -9,49 +9,100 @@
 /* - Esta forma de hacerlo hace que lo tenga CASI CASI, pero del 8 al 22 con la frase CUCÚ está colocado al principio en vez de en su sitio. ¿Qué hace que ocurra algo así? - */
 
 
-let horaCucu = 8
+/* let horaCucu = 8
 
- for(let horasDia = 0; horasDia < 24; horasDia++){
-     if(horasDia>7 && horasDia<23){
-        continue;
-    }
-     console.log(`Son las ${horasDia}:00`);
+  for(let horasDia = 0; horasDia < 24; horasDia++){
+      if(horasDia>7 && horasDia<23){
+         continue;
+     }
+      console.log(`Son las ${horasDia}:00`);
 
 
-        while(horaCucu<=22){
-        console.log(`Son las ${horaCucu}:00, CUCÚ!`);
-        horaCucu++;
-    }
-}
+         while(horaCucu<=22){
+         console.log(`Son las ${horaCucu}:00, CUCÚ!`);
+         horaCucu++;
+         
+     }
+} */
+
+// let horasDia = 0;
+
+// for(let horasCucu = 0; horasCucu < 23; horasCucu++){
+//     if(horasDia > 0  && horasDia < 8){
+//         continue;
+//     }  
+//     while(horasDia <=23){
+//         console.log(`Son las ${horasDia}:00`);
+//         horasCucu++; 
+ 
+//     }
+//     console.log(`Son las ${horasCucu}:00, CUCÚ!`)
+// }
+
+/* - ESTE ES LA FORMA QUE CASI CASI HACE QUE LO TENGA!! - */
+
+//   let horasDia = 0
+
+//   for(let horasCucu = 8; horasCucu <=22; horasCucu++){
+//       while(horasDia < 8){
+//           console.log(`Son las ${horasDia}:00`);
+//           horasDia++;
+//       }
+
+//       console.log(`Son las ${horasCucu}:00, CUCÚ!`);
+//   }
+
 
 
 /* - Con esta forma el bucle que creo es que, por cara hora generada en el while, se introduce en medio del 8 al 22 la oración 'Son las X, CUCÚ!' - */
 
-// let horasDia = 0;
-// let horasCucu = 8;
+//  let horasDia = 0;
+//  let horasCucu = 8;
 
-// while(horasDia<24){
-//     console.log(`Son las ${horasDia}:00`);
-//     horasDia++;
+//  while(horasDia<24){
+//      console.log(`Son las ${horasDia}:00`);
+//      horasDia++;
 // }   
 
-//     for(let horasCucu = 8; horasCucu < 23; horasCucu++){
-//         console.log(`Son las ${horasCucu}:00, CUCÚ!`);
+//      for(let horasCucu = 8; horasCucu <= 23; horasCucu++){
+//          console.log(`Son las ${horasCucu}:00, CUCÚ!`);
 // }
 
-/* - Otra de las dos formas más que he descifrado para intentar encontrar la solución a este ejercicio. - /*
+/* - Otra de las dos formas más que he descifrado para intentar encontrar la solución a este ejercicio. - */
 
-/* let horaCucu = 8
+// let horaCucu = 8
+// let horasDia = 0
 
-for(let horasDia = 0; horasDia < 24; horasDia++){
-   while(horaCucu<=22){
-       console.log(`Son las ${horaCucu}, CUCÚ!`);
-       horaCucu++;
-   }
-       if(horasDia>7 && horasDia<23){
-           continue;
-       }
-   console.log(`Son las ${horasDia}:00`);
-}
-*/
+// for(let horasDia = 0; horasDia < 24; horasDia++){
+//    while(horaCucu<=22){
+//        console.log(`Son las ${horaCucu}, CUCÚ!`);
+//        horaCucu++;
+//    }
+//    console.log(`Son las ${horasDia}:00`)
+// }
+
+
+//   for(let horasDia = 0; horasDia<24; horasDia++){
+//       if(horasDia >= 8 && horasDia <=22){
+//           console.log(`Son las ${horasDia}:00, CUCU!`)
+//       }
+//       else{
+//           console.log(`Son las ${horasDia}:00`)
+//       }
+//}
+
+
+
+
+for(let i=0; i<24; i++){
+
+    let hora = i;
+    let mensaje = "Son las " + hora + ":00,";
+
+    if(i>=8 && i<23){
+        mensaje = mensaje + " CUCÚ!";
+    }
+    console.log(mensaje);
+ }
+
 
