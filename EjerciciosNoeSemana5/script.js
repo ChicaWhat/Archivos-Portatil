dc75c9e31ba052e6f7846ebe6c96ec0ad53a4c38
Carmen González <carmengh106@gmail.com>
2023-12-19 15:58:29 +0100

    Explicación ejercicio 1

--- EjerciciosNoeSemana5/script.js
+++ EjerciciosNoeSemana5/script.js
@@ -8,11 +8,15 @@
 
 */
 
+// - Ejercicio 01 - //
 
 function saludarUsuario (nombre) {
     let mensaje = `Bienvenid@ ${nombre}`;
     console.log(`${mensaje}`);
-
 }
 
 saludarUsuario('Sofi')
+
+/*  He creado una función cuyo nombre es saludarUsuario. El parámetro que voy a declarar es 'nombre'. Dentro de la función creo una variable mensaje que va a contener
+un string + el parámetro de mi función. Esta variable aparecerá en consola con mi console.log. Cierro la función, porque ya he definido lo que quiero que llevo a cabo.
+Fuera de esta, aparecerá la variable mensaje que he creado para saludarUsuario tantas veces como yo quiera cada vez que mencione a la función. */
