79b1175e923ee4a69814dee85265c27a80987524
Carmen González <carmengh106@gmail.com>
2023-12-19 15:50:54 +0100

    Primer ejercicio realizado

--- EjerciciosNoeSemana5/script.js
+++ EjerciciosNoeSemana5/script.js
@@ -1,2 +1,18 @@
 'use strict'
 
+/* EJERCICIOS DE TAREA:
+
+    1er ejercicio: Saludar a un usuario a través de su nombre utilizando function.
+    2do ejercicio: Crear un function que permita sacar el 10% de un número dado
+    3er ejercicio: Calcular el área de un triángulo (base*altura/2)
+
+*/
+
+
+function saludarUsuario (nombre) {
+    let mensaje = `Bienvenid@ ${nombre}`;
+    console.log(`${mensaje}`);
+
+}
+
+saludarUsuario('Sofi')
