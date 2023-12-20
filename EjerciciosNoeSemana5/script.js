9ed52c78f74034f9e22913f5c7fb42cea408ad29
Carmen González <carmengh106@gmail.com>
2023-12-20 17:36:07 +0100

    Haciendo una segunda version del ejercicio 01

--- EjerciciosNoeSemana5/script.js
+++ EjerciciosNoeSemana5/script.js
@@ -10,16 +10,32 @@
 
 // - Ejercicio 01 - //
 
-function saludarUsuario (nombre) {
+
+/* function saludarUsuario (nombre) {
+
     let mensaje = `Bienvenid@ ${nombre}`;
-    console.log(`${mensaje}`);
+    console.log(mensaje);
+
 }
 
 saludarUsuario('Sofi');
-saludarUsuario("Pepito Grillo");
+saludarUsuario("Pepito Grillo");  */
 
 /*  He creado una función cuyo nombre es saludarUsuario. El parámetro que voy a declarar es 'nombre'. Dentro de la función creo una variable mensaje que va a contener un string + el parámetro de mi función. Esta variable aparecerá en consola con mi console.log. Cierro la función, porque ya he definido lo que quiero que lleve a cabo.
 Fuera de esta, nombraré a mi función tantas veces como yo quiera con el argumento que quiera que aparezca dentro de la variable mensaje. */
+
+
+function saludarUsuario () {
+
+    let mensaje = prompt("Introduzca su nombre de usuario");
+    let saludo = `Bienvenid@ ${mensaje}`;
+    console.log(saludo);
+}
+
+saludarUsuario();
+
+
+
 
 // - Ejercicio 02 - //
 
@@ -28,9 +44,9 @@ Fuera de esta, nombraré a mi función tantas veces como yo quiera con el argume
 // Sacara el 10% de 100 es 90. Cómo enfrento esto en una función? Este es un ejemplo, pero yo quiero que número = x
 
 
-function porcentaje(num1) {
+ function porcentaje(num1) {
     let cuenta = num1 * 0.1;
-    let resultado = /* num1 */ - cuenta;
+    let resultado =  num1  - cuenta;
     console.log(resultado);
 }
 
@@ -47,6 +63,6 @@ function areaTriangulo (base, altura) {
 }
 
 areaTriangulo(10, 7);
-areaTriangulo(20, 5);
+areaTriangulo(20, 5); 
 
 // No puede ser tan fácil... 
