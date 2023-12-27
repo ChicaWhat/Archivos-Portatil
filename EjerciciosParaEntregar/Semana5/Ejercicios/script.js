1ea8557ea2afa4aff5273f0f4599e6da66f7a304
Carmen González <carmengh106@gmail.com>
2023-12-27 02:07:16 +0100

    Resueltos todos los ejercicios de la semana 5

--- EjerciciosParaEntregar/Semana5/Ejercicios/script.js
+++ EjerciciosParaEntregar/Semana5/Ejercicios/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// - Ejercicio 1 - //
+
 const people = [
   {
     name: "Maria",
@@ -62,7 +64,7 @@ for ( let persona of people){
   imprimir(persona.name, persona.age);
 } */
 
-
+// - Ejercicio 2 - //
 
 const names = [
   "A-Jay",
@@ -102,10 +104,11 @@ Debería imprimir esto: */
   return arrayLista;
 } 
 
-
 console.log(quitarDuplicados(names));   */
 
 
+
+// - Ejercicio 3 - //
 
   const pizzas = [
   "margarita",
@@ -117,7 +120,7 @@ console.log(quitarDuplicados(names));   */
 ];
 
 
-function combine(pizzas) {
+/* function combine(pizzas) {
     const combinations = [];
 
     // Escribe aquí tu código
@@ -137,7 +140,216 @@ function combine(pizzas) {
     return combinations;
 }
 
-console.log(combine(pizzas));
+console.log(combine(pizzas)); */
+
+// - Ejercicio 4 - //
+
+const rickandmortyCharacters = [
+  {
+    id: 1,
+    name: "Rick Sanchez",
+    status: "Alive",
+    species: "Human",
+    gender: "Male",
+    origin: {
+      name: "Earth (C-137)",
+      url: "https://rickandmortyapi.com/api/location/1",
+    },
+    location: {
+      name: "Citadel of Ricks",
+      url: "https://rickandmortyapi.com/api/location/3",
+    },
+    image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    url: "https://rickandmortyapi.com/api/character/1",
+    created: "2017-11-04T18:48:46.250Z",
+  },
+  {
+    id: 2,
+    name: "Morty Smith",
+    status: "Alive",
+    species: "Human",
+    gender: "Male",
+    origin: {
+      name: "unknown",
+      url: "",
+    },
+    location: {
+      name: "Citadel of Ricks",
+      url: "https://rickandmortyapi.com/api/location/3",
+    },
+    image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+
+    url: "https://rickandmortyapi.com/api/character/2",
+    created: "2017-11-04T18:50:21.651Z",
+  },
+  {
+    id: 3,
+    name: "Summer Smith",
+    status: "Alive",
+    species: "Human",
+    gender: "Female",
+    origin: {
+      name: "Earth (Replacement Dimension)",
+      url: "https://rickandmortyapi.com/api/location/20",
+    },
+    location: {
+      name: "Earth (Replacement Dimension)",
+      url: "https://rickandmortyapi.com/api/location/20",
+    },
+    image: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+    url: "https://rickandmortyapi.com/api/character/3",
+    created: "2017-11-04T19:09:56.428Z",
+  },
+  {
+    id: 4,
+    name: "Beth Smith",
+    status: "Alive",
+    species: "Human",
+    gender: "Female",
+    origin: {
+      name: "Earth (Replacement Dimension)",
+      url: "https://rickandmortyapi.com/api/location/20",
+    },
+    location: {
+      name: "Earth (Replacement Dimension)",
+      url: "https://rickandmortyapi.com/api/location/20",
+    },
+    image: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+    url: "https://rickandmortyapi.com/api/character/4",
+    created: "2017-11-04T19:22:43.665Z",
+  },
+  {
+    id: 5,
+    name: "Jerry Smith",
+    status: "Alive",
+    species: "Human",
+    gender: "Male",
+    origin: {
+      name: "Earth (Replacement Dimension)",
+      url: "https://rickandmortyapi.com/api/location/20",
+    },
+    location: {
+      name: "Earth (Replacement Dimension)",
+      url: "https://rickandmortyapi.com/api/location/20",
+    },
+    image: "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+    url: "https://rickandmortyapi.com/api/character/5",
+    created: "2017-11-04T19:26:56.301Z",
+  },
+  {
+    id: 6,
+    name: "Abadango Cluster Princess",
+    status: "Alive",
+    species: "Alien",
+    gender: "Female",
+    origin: {
+      name: "Abadango",
+      url: "https://rickandmortyapi.com/api/location/2",
+    },
+    location: {
+      name: "Abadango",
+      url: "https://rickandmortyapi.com/api/location/2",
+    },
+    image: "https://rickandmortyapi.com/api/character/avatar/6.jpeg",
+    url: "https://rickandmortyapi.com/api/character/6",
+    created: "2017-11-04T19:50:28.250Z",
+  },
+  {
+    id: 7,
+    name: "Abradolf Lincler",
+    status: "unknown",
+    species: "Human",
+    gender: "Male",
+    origin: {
+      name: "Earth (Replacement Dimension)",
+      url: "https://rickandmortyapi.com/api/location/20",
+    },
+    location: {
+      name: "Testicle Monster Dimension",
+      url: "https://rickandmortyapi.com/api/location/21",
+    },
+    image: "https://rickandmortyapi.com/api/character/avatar/7.jpeg",
+    url: "https://rickandmortyapi.com/api/character/7",
+    created: "2017-11-04T19:59:20.523Z",
+  },
+  {
+    id: 8,
+    name: "Adjudicator Rick",
+    status: "Dead",
+    species: "Human",
+    gender: "Male",
+    origin: {
+      name: "unknown",
+      url: "",
+    },
+    location: {
+      name: "Citadel of Ricks",
+      url: "https://rickandmortyapi.com/api/location/3",
+    },
+    image: "https://rickandmortyapi.com/api/character/avatar/8.jpeg",
+    url: "https://rickandmortyapi.com/api/character/8",
+    created: "2017-11-04T20:03:34.737Z",
+  },
+  {
+    id: 9,
+    name: "Agency Director",
+    status: "Dead",
+    species: "Human",
+    gender: "Male",
+    origin: {
+      name: "Earth (Replacement Dimension)",
+      url: "https://rickandmortyapi.com/api/location/20",
+    },
+    location: {
+      name: "Earth (Replacement Dimension)",
+      url: "https://rickandmortyapi.com/api/location/20",
+    },
+    image: "https://rickandmortyapi.com/api/character/avatar/9.jpeg",
+    url: "https://rickandmortyapi.com/api/character/9",
+    created: "2017-11-04T20:06:54.976Z",
+  },
+  {
+    id: 10,
+    name: "Alan Rails",
+    status: "Dead",
+    species: "Human",
+    gender: "Male",
+    origin: {
+      name: "unknown",
+      url: "",
+    },
+    location: {
+      name: "Worldender's lair",
+      url: "https://rickandmortyapi.com/api/location/4",
+    },
+    image: "https://rickandmortyapi.com/api/character/avatar/10.jpeg",
+    url: "https://rickandmortyapi.com/api/character/10",
+    created: "2017-11-04T20:19:09.017Z",
+  },
+];
+
+console.log(rickandmortyCharacters);
+
+// *rickandmortyCharacters
+
+// Escribe aquí tu código
+
+// *Voy a crear un nuevo array con las propiedades name, status, species y gender
+
+const nuevoArrayRickandMorty = rickandmortyCharacters.map( (rickandmortyCharacter) => {
+  return {
+    name: rickandmortyCharacter.name,
+    status: rickandmortyCharacter.status,
+    species: rickandmortyCharacter.species,
+    gender: rickandmortyCharacter.gender
+  }
+});
+
+console.log(nuevoArrayRickandMorty);
+
+const AliveRickandMorty = nuevoArrayRickandMorty.filter( nuevoArrayRM => nuevoArrayRM.status === 'Alive' );
+
+console.log(AliveRickandMorty);
 
 
 
