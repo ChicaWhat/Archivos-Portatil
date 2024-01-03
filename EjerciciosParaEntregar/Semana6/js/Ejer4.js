25f342a051269d5a821f88522993532589bc5034
Carmen González <carmengh106@gmail.com>
2024-01-03 22:58:23 +0100

    Solución ejercicio 4 semana 6

--- EjerciciosParaEntregar/Semana6/js/Ejer4.js
+++ EjerciciosParaEntregar/Semana6/js/Ejer4.js
@@ -1,1 +1,27 @@
 'use strict'
+
+function myFuncion() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const random = Math.floor(Math.random() * 10);
+        if (random >= 5) {
+          resolve(`Todo bien: ${random}`);
+        } else {
+          reject(`Tenemos un problema: ${random}`);
+        }
+      }, 2000);
+    });
+}
+
+//* THEN - CATCH *//
+
+myFuncion()
+    .then((resultado) => {
+        console.log(resultado);
+    })
+    .catch((error) => {
+        console.error(error);
+    })
+    .finally(() => {
+        console.log("Prueba completada!");
+    })
